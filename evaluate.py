2bf97e4c2d48d12541e08534321c378da6b9bba7
esnaiqu <qiansun@brandeis.edu>
2021-08-03 15:40:29 +0800

    started separate modules for run(read graph and output embedding) and evaluate(link/node prediction and evaluation), run.py currently works well with n2v, next step is to make it also work with deepwalk, and then work on evaluate.py.

---
+++ evaluate.py
@@ -0,0 +1,7 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, average_precision_score, f1_score, roc_auc_score
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.preprocessing import MultiLabelBinarizer
+
+from deepwalk import
+def link_prediction()
