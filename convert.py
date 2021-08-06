488f086d46ad2c0d0caa667f1e0238d5549f76e7
esnaiqu <qiansun@brandeis.edu>
2021-08-06 14:04:13 +0800

    debug for convert.py, now it works well(given a .mat file, generate both .edgelist and .adjlist files to the output folder), also added the conversion output for blogcatalog.mat

--- convert.py
+++ convert.py
@@ -1,20 +1,48 @@
 import networkx as nx
 import argparse
 from run import read_graph
-from scipy import io
+import numpy as np
 
 def parse_args():
 
-    parser = argparse.ArgumentParser(description="Convert adjlist or edgelist graph to sparse matrix .mat file")
+    parser = argparse.ArgumentParser(description="Convert .mat file to adjlist and edgelist graphs.")
 
-    parser.add_argument('--input', nargs='?', default='examples/karate.edgelist', help='Input graph path')
+    parser.add_argument('--input', nargs='?', default='examples/blogcatalog.mat', help='Input .mat path')
 
-    parser.add_argument('--format', nargs='?', default='edgelist', help='Select from edgelist and adjlist')
+    parser.add_argument('--weighted', dest='weighted', action='store_true', help='Boolean specifying (un)weighted. Default is unweighted.')
+    parser.add_argument('--unweighted', dest='unweighted', action='store_false')
+    parser.set_defaults(weighted=False)
+
+    parser.add_argument('--directed', dest='directed', action='store_true', help='Graph is (un)directed. Default is undirected.')
+    parser.add_argument('--undirected', dest='undirected', action='store_false')
+    parser.set_defaults(directed=False)
 
     return parser.parse_args()
 
 def main(args):
     in_fname = args.input
     G = read_graph(args)
-    M = nx.to_scipy_sparse_matrix(G, dtype=int, format='coo')
-    io.savemat('output/'+in_fname[in_fname.find('/'):in_fname.find('.')]+'-converted.mat', {'M': M})
+
+    edgelist_fname = 'converted/'+in_fname[in_fname.find('/')+1:in_fname.find('.')]+'-converted.edgelist'
+    adjlist_fname = 'converted/' + in_fname[in_fname.find('/') + 1:in_fname.find('.')] + '-converted.adjlist'
+
+    e_list = nx.convert.to_edgelist(G)
+    a_list = nx.to_dict_of_lists(G)
+
+    edgelist_out = open(edgelist_fname, "w")
+    for edge in e_list:
+        edgelist_out.write(' '.join(str(e) for e in edge) + '\n')
+    edgelist_out.close()
+    print("file saved to {}".format(edgelist_fname))
+
+    adjlist_out = open(adjlist_fname, "w")
+    for adj in a_list.keys():
+        adjlist_out.write(str(adj) + ' ' + ' '.join(str(a) for a in a_list[adj]) + '\n')
+    adjlist_out.close()
+    print("file saved to {}".format(adjlist_fname))
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    args.format = 'mat'
+    main(args)
