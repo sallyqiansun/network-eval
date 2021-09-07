b9bf4e62134fa681afd5ddce9a094814a8d44ac2
Qian Sun <qiansun@brandeis.edu>
2021-09-07 11:51:34 +0800

    fixed bugs in evaluate-LinkPrediction

--- evaluate-LinkPrediction.py
+++ evaluate-LinkPrediction.py
@@ -21,12 +21,12 @@ def node2vec_embedding(args, graph):
 
     model = Word2Vec(
         walks,
-        size=args.dimensions,
+        vector_size=args.representation_size,
         window=args.window_size,
         min_count=0,
         sg=1,
         workers=args.workers,
-        iter=args.num_iter,
+        iter=args.iter,
     )
 
     def get_embedding(u):
@@ -121,9 +121,6 @@ def main():
     parser.add_argument('--iter', default=1, type=int,
                         help='Number of epochs in SGD')
 
-    parser.add_argument('--workers', type=int, default=8,
-                        help='Number of parallel processes. Default is 8.')
-
     parser.add_argument("--all", default=False, action='store_true',
                         help='The embeddings are evaluated on all training percents from 10 to 90 when this flag is set to true. '
                              'By default, only training percents of 10, 50 and 90 are used.')
@@ -132,8 +129,6 @@ def main():
                         help='Network graph')
 
     args = parser.parse_args()
-
-    binary_operators = [operator_hadamard, operator_l1, operator_l2, operator_avg]
 
     # data
     matfile = args.network
@@ -150,13 +145,15 @@ def main():
 
     graph = StellarGraph.from_networkx(G)
 
-    # test
+    # graph_test: for computing test node embeddings with more edges than the graph_train
+    # examples_test: test set of + and - edges not used for computing the test node embeddings or for classifier training or model selection
     edge_splitter_test = EdgeSplitter(graph)
     graph_test, examples_test, labels_test = edge_splitter_test.train_test_split(
         p=0.1, method="global"
     )
 
-    # train
+    # graph_train: for computing node embeddings
+    # examples: training set of + and - edges not used for computing node embeddings
     edge_splitter_train = EdgeSplitter(graph_test, graph)
     graph_train, examples, labels = edge_splitter_train.train_test_split(
         p=0.1, method="global"
@@ -168,6 +165,10 @@ def main():
 
     #TODO: add other methods here
 
+    # 1. calculate link/edge emb for + and - samples by applying a binary operator
+    # 2. given the emb, train a logistic regression classifier to predict whether an edge between two nodes should exist or not.
+    # 3. evaluate the performance of the classifier for each of the 4 operators on the training data with emb on graph_train, and select the best classifier
+    # 4. the best classifier is then used to calculate scores on the test data with node embeddings calculated on graph_test
 
     if args.all:
         training_percents = np.asarray(range(1, 10)) * .1
@@ -184,10 +185,11 @@ def main():
             clf,
             examples_model_selection,
             labels_model_selection,
-            embedding_test,
+            embedding_train,
             args.operator,
         )
-        all_results[train_percent].append(score)
+
+        all_results[train_percent] = score
 
     eva_fname = 'evaluation/'+ matfile[matfile.find('/'):matfile.find('.mat')] + '-' + 'n2v.txt'
     sys.stdout = open(eva_fname, "w")
