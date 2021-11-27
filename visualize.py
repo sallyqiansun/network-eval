7d59f7dd4f7cfe8293f0c3fa2d6d942fb9f267b7
Qian Sun <qiansun@brandeis.edu>
2021-11-27 16:57:04 +0800

    commit changes made during last meeting i.e. update plot with TSNE

--- visualize.py
+++ visualize.py
@@ -4,14 +4,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.manifold import TSNE
 import networkx as nx
+from sklearn.manifold import TSNE
 
 def visualize(result, config):
     X = result[1]
+    X = TSNE(n_components=2).fit_transform(X)
     predicted_on_all = result[2]
     true_label_on_all = result[3]
 
     plt.rcParams["figure.figsize"] = (40,10)
-    plt.subplot(1, 2, 1)
+    plt.subplot(1, 3, 1)
     plt.scatter(
         X[:, 0],
         X[:, 1],
@@ -21,7 +23,7 @@ def visualize(result, config):
     )
     plt.title('{} embedding with true labels'.format(config["method"]))
 
-    plt.subplot(1, 2, 2)
+    plt.subplot(1, 3, 2)
     plt.scatter(
         X[:, 0],
         X[:, 1],
@@ -31,10 +33,31 @@ def visualize(result, config):
     )
     plt.title('{} embedding with predicted labels'.format(config["method"]))
 
+
+
+
+
+    w_b = [1 if x == y else 0 for x, y, in zip(result[2], result[3])]
+
+    plt.subplot(1, 3, 3)
+    plt.scatter(
+        X[:, 0],
+        X[:, 1],
+        c=w_b,
+        cmap="jet",
+        alpha=0.7,
+    )
+    plt.title('{} difference'.format(config["method"]))
+
+
     plt.savefig(config["fig-path"])
 
 
+# import task
 # import json
 # config_file = open("config.json", "r")
 # config = json.load(config_file)
-# visualize(config)
+# data_path = "data/" + config["data"] + ".gpickle"
+# G = nx.read_gpickle(data_path)
+# result = task.train(G, config)
+# visualize(result, config)
