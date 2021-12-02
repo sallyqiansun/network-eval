ab26ac72442b273f0678319e2b977f5ac7a8bd9d
Qian Sun <qiansun@brandeis.edu>
2021-12-03 00:25:24 +0800

    updated matrix factorization on Cora

--- matfact.py
+++ matfact.py
@@ -2,23 +2,33 @@ import networkx as nx
 import numpy as np
 
 def run(config, G):
+    nodes = sorted(G.nodes())
+    #save original node_ids using dictionary
+    node_dict = {}
+    i = 0
+    while i < len(nodes):
+        node_dict[i] = nodes[i]
+        i += 1
+
     k = config['dimensions']
     A = np.array(nx.adjacency_matrix(G).todense())
     D = np.diag(np.sum(A, axis=1))
     I = np.identity(A.shape[0])
     D_inv_sqrt = np.linalg.inv(np.sqrt(D))
-    L = I - np.dot(D_inv_sqrt, A).dot(D_inv_sqrt)
+    L = I - np.matmul(np.matmul(D_inv_sqrt, A), D_inv_sqrt)
     e, v = np.linalg.eig(L)
-    sorted = np.argsort(e)
-    ind = sorted[e[sorted] != 0][:]
-    emb = v[ind]
+    e = np.real(e)
+    v = np.real(v)
+    ind = np.argsort(e)[:k]
+    # ind = sorted[e[sorted] != 0][:]
+    emb = v[ind].T
     f = open(config['emb-path'], "w")
     f.write("{} {}\n".format(emb.shape[0], emb.shape[1]))
-    for i in ind:
-        if i < 2706:
-            f.write("{} ".format(i+1))
-            for e in emb[i]:
-                f.write("{} ".format(e))
-            f.write("\n")
+    emb = emb.tolist()
+    for i in range (len(emb)):
+        f.write("{} ".format(node_dict[i]))
+        for e in emb[i]:
+            f.write("{} ".format(e))
+        f.write("\n")
     f.close()
     print("Embedding saved to {}.".format(config['emb-path']))
