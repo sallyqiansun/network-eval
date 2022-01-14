7b5389e25e3197fac40b4361f29a493873482a6f
Qian Sun <qiansun@brandeis.edu>
2022-01-14 21:02:03 +0800

    dgi to be debugged

---
+++ dgi.py
@@ -0,0 +1,230 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import networkx as nx
+import scipy.sparse as sp
+
+class AvgReadout(nn.Module):
+    def __init__(self):
+        super(AvgReadout, self).__init__()
+
+    def forward(self, seq, msk):
+        if msk is None:
+            return torch.mean(seq, 1)
+        else:
+            msk = torch.unsqueeze(msk, -1)
+            return torch.sum(seq * msk, 1) / torch.sum(msk)
+class Discriminator(nn.Module):
+    def __init__(self, n_h):
+        super(Discriminator, self).__init__()
+        self.f_k = nn.Bilinear(n_h, n_h, 1)
+
+        for m in self.modules():
+            self.weights_init(m)
+
+    def weights_init(self, m):
+        if isinstance(m, nn.Bilinear):
+            torch.nn.init.xavier_uniform_(m.weight.data)
+            if m.bias is not None:
+                m.bias.data.fill_(0.0)
+
+    def forward(self, c, h_pl, h_mi, s_bias1=None, s_bias2=None):
+        c_x = torch.unsqueeze(c, 1)
+        c_x = c_x.expand_as(h_pl)
+
+        sc_1 = torch.squeeze(self.f_k(h_pl, c_x), 2)
+        sc_2 = torch.squeeze(self.f_k(h_mi, c_x), 2)
+
+        if s_bias1 is not None:
+            sc_1 += s_bias1
+        if s_bias2 is not None:
+            sc_2 += s_bias2
+
+        logits = torch.cat((sc_1, sc_2), 1)
+
+        return logits
+class DGI(nn.Module):
+    def __init__(self, n_in, n_h, activation):
+        super(DGI, self).__init__()
+        self.gcn = GCN(n_in, n_h, activation)
+        self.read = AvgReadout()
+
+        self.sigm = nn.Sigmoid()
+
+        self.disc = Discriminator(n_h)
+
+    def forward(self, seq1, seq2, adj, sparse, msk, samp_bias1, samp_bias2):
+        h_1 = self.gcn(seq1, adj, sparse)
+
+        c = self.read(h_1, msk)
+        c = self.sigm(c)
+
+        h_2 = self.gcn(seq2, adj, sparse)
+
+        ret = self.disc(c, h_1, h_2, samp_bias1, samp_bias2)
+
+        return ret
+
+    # Detach the return variables
+    def embed(self, seq, adj, sparse, msk):
+        h_1 = self.gcn(seq, adj, sparse)
+        c = self.read(h_1, msk)
+
+        return h_1.detach(), c.detach()
+
+
+class GCN(nn.Module):
+    def __init__(self, in_ft, out_ft, act, bias=True):
+        super(GCN, self).__init__()
+        self.fc = nn.Linear(in_ft, out_ft, bias=False)
+        self.act = nn.PReLU() if act == 'prelu' else act
+
+        if bias:
+            self.bias = nn.Parameter(torch.FloatTensor(out_ft))
+            self.bias.data.fill_(0.0)
+        else:
+            self.register_parameter('bias', None)
+
+        for m in self.modules():
+            self.weights_init(m)
+
+    def weights_init(self, m):
+        if isinstance(m, nn.Linear):
+            torch.nn.init.xavier_uniform_(m.weight.data)
+            if m.bias is not None:
+                m.bias.data.fill_(0.0)
+
+    # Shape of seq: (batch, nodes, features)
+    def forward(self, seq, adj, sparse=False):
+        seq_fts = self.fc(seq)
+        if sparse:
+            out = torch.unsqueeze(torch.spmm(adj, torch.squeeze(seq_fts, 0)), 0)
+        else:
+            out = torch.bmm(adj, seq_fts)
+        if self.bias is not None:
+            out += self.bias
+
+        return self.act(out)
+
+
+
+def sparse_to_tuple(sparse_mx, insert_batch=False):
+    """Convert sparse matrix to tuple representation."""
+    """Set insert_batch=True if you want to insert a batch dimension."""
+    def to_tuple(mx):
+        if not sp.isspmatrix_coo(mx):
+            mx = mx.tocoo()
+        if insert_batch:
+            coords = np.vstack((np.zeros(mx.row.shape[0]), mx.row, mx.col)).transpose()
+            values = mx.data
+            shape = (1,) + mx.shape
+        else:
+            coords = np.vstack((mx.row, mx.col)).transpose()
+            values = mx.data
+            shape = mx.shape
+        return coords, values, shape
+
+    if isinstance(sparse_mx, list):
+        for i in range(len(sparse_mx)):
+            sparse_mx[i] = to_tuple(sparse_mx[i])
+    else:
+        sparse_mx = to_tuple(sparse_mx)
+
+    return sparse_mx
+
+def preprocess_features(features):
+    """Row-normalize feature matrix and convert to tuple representation"""
+    rowsum = np.array(features.sum(1))
+    r_inv = (1 / rowsum).flatten()
+    r_inv[np.isinf(r_inv)] = 0.
+    r_mat_inv = sp.diags(r_inv)
+    features = r_mat_inv.dot(features)
+    return features.todense(), sparse_to_tuple(features)
+
+def sparse_mx_to_torch_sparse_tensor(sparse_mx):
+    """Convert a scipy sparse matrix to a torch sparse tensor."""
+    sparse_mx = sp.coo_matrix(sparse_mx).astype(np.float32)
+    indices = torch.from_numpy(
+        np.vstack((sparse_mx.row, sparse_mx.col)).astype(np.int64))
+    values = torch.from_numpy(sparse_mx.data)
+    shape = torch.Size(sparse_mx.shape)
+    return torch.sparse.FloatTensor(indices, values, shape)
+
+def normalize_adj(adj):
+    """Symmetrically normalize adjacency matrix."""
+    adj = sp.coo_matrix(adj)
+    rowsum = np.array(adj.sum(1))
+    d_inv_sqrt = np.power(rowsum, -0.5).flatten()
+    d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0.
+    d_mat_inv_sqrt = sp.diags(d_inv_sqrt)
+    return adj.dot(d_mat_inv_sqrt).transpose().dot(d_mat_inv_sqrt).tocoo()
+
+def preprocess_adj(adj):
+    """Preprocessing of adjacency matrix for simple GCN model and conversion to tuple representation."""
+    adj_normalized = normalize_adj(adj + sp.eye(adj.shape[0]))
+    return sparse_to_tuple(adj_normalized)
+
+def run(G, config):
+    adj = nx.adjacency_matrix(G)
+    adj = normalize_adj(adj + sp.eye(adj.shape[0]))
+    feat = nx.get_node_attributes(G, "feature")
+    a = np.array(list(feat.values()))
+    features, _ = preprocess_features(sp.csr_matrix(a))
+    ft_size = features.shape[1]
+    # lab = nx.get_node_attributes(G, "label")
+    # labels = np.array(lab.values())
+
+    sp_adj = sparse_mx_to_torch_sparse_tensor(adj)
+    features = torch.FloatTensor(features[np.newaxis])
+
+    model = DGI(ft_size, config['hidden-channels'], activation='prelu')
+    optimiser = torch.optim.Adam(model.parameters(), lr=config['learning-rate'])
+    xent = nn.CrossEntropyLoss()
+
+    for epoch in range(config['iter']):
+        model.train()
+        optimiser.zero_grad()
+
+        idx = np.random.permutation(features.shape[0])
+        shuf_fts = features[:, idx, :]
+
+        lbl_1 = torch.ones(config['batch-size'], features.shape[0])
+        lbl_2 = torch.zeros(config['batch-size'], features.shape[0])
+        lbl = torch.cat((lbl_1, lbl_2), 1)
+
+        print(features.shape)
+        print(shuf_fts.shape)
+        print(sp_adj.shape)
+        print(lbl.shape)
+
+
+        logits = model(features, shuf_fts, sp_adj, True, None, None, None)
+        print(logits.shape)
+        print(lbl.shape)
+
+        loss = xent(logits, lbl)
+
+        print('Loss:', loss)
+
+        if loss < best:
+            best = loss
+            best_t = epoch
+            cnt_wait = 0
+            torch.save(model.state_dict(), 'best_dgi.pkl')
+        else:
+            cnt_wait += 1
+
+        loss.backward()
+        optimiser.step()
+
+    print('Loading {}th epoch'.format(best_t))
+    model.load_state_dict(torch.load('best_dgi.pkl'))
+
+    embeds, _ = model.embed(adj, False, None)
+
+import json
+config_file = open('config.json', "r")
+config = json.load(config_file)
+data_path = "data/" + config["data"] + ".gpickle"
+graph = nx.read_gpickle(data_path)
+run(graph, config)
